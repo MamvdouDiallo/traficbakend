8eee093bd86d283bba4924a78b08cbc209908b76
salioufereya <salioufereya19@gmail.com>
2025-04-19 18:30:16 +0000

    set Token 15minutes

--- src/main/java/com/itma/gestionProjet/security/SecurityConstants.java
+++ src/main/java/com/itma/gestionProjet/security/SecurityConstants.java
@@ -1,5 +1,5 @@
 package com.itma.gestionProjet.security;
 
 public class SecurityConstants {
-    public static final long JWT_EXPIRATION = 70000;
+    public static final long JWT_EXPIRATION = 15 * 60 * 1000;
 }
